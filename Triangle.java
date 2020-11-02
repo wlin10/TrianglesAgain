08869c86d975352ed6b270da281d42fdabae7d3e
wlin10 <wlin10@stuy.edu>
2020-11-02 00:41:57 -0500

    perimeter

---
+++ Triangle.java
@@ -0,0 +1,23 @@
+public class Triangle{
+
+  private Point v1, v2, v3;
+
+  public Triangle(Point a, Point b, Point c) {
+    v1 = a;
+    v2 = b;
+    v3 = c;
+  }
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+    v1 = new Point(x1, y1);
+    v2 = new Point(x2, y2);
+    v3 = new Point(x3, y3);
+  }
+
+  public double getPerimeter() {
+    double sideA = v1.distanceTo(v2);
+    double sideB = v2.distanceTo(v3);
+    double sideC = v3.distanceTo(v1);
+    return (sideA + sideB + sideC);
+  }
+
+}
