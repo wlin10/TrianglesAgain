117a2d22ec38313d1dbff29912ac6528dbbb28ac
wlin10 <wlin10@stuy.edu>
2020-11-02 00:45:30 -0500

    peri and area

--- Tester.java
+++ Tester.java
@@ -7,12 +7,21 @@ public class Tester{
 		Point pointD = new Point(14,6);
 		Point pointE = new Point(5,8);
 
+		Triangle triangleA = new Triangle(pointA, pointD, pointE);
+		Triangle triangleB = new Triangle(6, 32, 5, 16, 25, 13);
+
 		System.out.println(pointA.distanceTo(pointB));
 		System.out.println(pointB.distanceTo(pointC));
 		System.out.println(pointA.distanceTo(pointE));
 
 		System.out.println(pointA.equals(pointB));
 		System.out.println(pointC.equals(pointB));
+
+		System.out.println(triangleA.getPerimeter());
+		System.out.println(triangleB.getPerimeter());
+
+		System.out.println(triangleA.getArea());
+		System.out.println(triangleB.getArea());
 
 	}
 }
