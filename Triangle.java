a28f2c29305d7938483eb481bda5d84f4fa9c341
wlin10 <wlin10@stuy.edu>
2020-11-02 01:00:13 -0500

    classify and toString

--- Triangle.java
+++ Triangle.java
@@ -19,12 +19,38 @@ public class Triangle{
     double sideC = v3.distanceTo(v1);
     return (sideA + sideB + sideC);
   }
+
   public double getArea() {
     double sideA = v1.distanceTo(v2);
     double sideB = v2.distanceTo(v3);
     double sideC = v3.distanceTo(v1);
     double semi = ((sideA+sideB+sideC)/2);
     return (Math.sqrt((semi)*(semi-sideA)*(semi-sideB)*(semi-sideC)));
+  }
+
+  public String classify() {
+    double sideA = (Math.round(v1.distanceTo(v2) * 10000)/10000);
+    double sideB = (Math.round(v2.distanceTo(v3) * 10000)/10000);
+    double sideC = (Math.round(v3.distanceTo(v1) * 10000)/10000);
+    if (sideA == sideB) {
+      if (sideA == sideC) {
+        return ("Equilateral");
+      } else {
+        return ("Isoceles");
+      }
+    } else if (sideA == sideC) {
+      return ("Isoceles");
+    } else if (sideB == sideC) {
+      return ("Isoceles");
+    } else {
+      return ("Scalene");
+    }
+  }
+
+  public String toString() {
+    return ("v1(" + v1.getX() + ", " + v1.getY() + ")" +
+    " v2(" + v2.getX() + ", " + v2.getY() + ")" +
+    " v3(" + v3.getX() + ", " + v3.getY() + ")");
   }
 
 }
